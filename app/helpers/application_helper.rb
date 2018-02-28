47eaaadb95bedb5e65a0977d42012903387341c4
YuoMamoru <myuo@cam.hi-ho.ne.jp>
2018-02-28 23:39:19 +0900

    :+1: Rebuild base64_image_tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,13 @@
 require "base64"
 
 module ApplicationHelper
-  def base64_image_tag(image)
+  def base64_image_tag(image, options = {})
     return "" if image.nil?
-    "<img src=\"#{image.base64_src}\" />".html_safe
+    if options[:width] && options[:height].blank? && options[:size].blank?
+      options[:height] = options[:with].to_i * image.height / image.width
+    elsif options[:height] && options[:width].blank? && options[:size].blank?
+      options[:width] = options[:height].to_i * image.width / image.height
+    end
+    image_tag(image.base64_src, options)
   end
 end
