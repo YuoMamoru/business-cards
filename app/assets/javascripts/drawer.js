f006233cfe765ce838fecf86948f8334ca345aa1
YuoMamoru <myuo@cam.hi-ho.ne.jp>
2018-03-08 22:18:00 +0900

    :+1: Add drawer

---
+++ app/assets/javascripts/drawer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+document.addEventListener('turbolinks:load', (evt) => {
+  const drawerElm = document.querySelector('.res-drawer');
+  const drawer = new mdc.drawer.MDCPersistentDrawer(drawerElm);
+  document.querySelector('.res-main-menu').addEventListener('click', (e) => {
+    drawer.open = !drawer.open;
+  }, false);
+}, false);
---
+++ spec/helpers/mdc_hepler_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe MdcHelper do
+  include MdcHelper
+
+  describe "#merge_class_name" do
+    it "adds class attribute if option have no class attribute" do
+      options = { id: "elm_id" }
+      merge_class_name(options, "foo")
+      expect(options[:class]).to eq "foo"
+    end
+
+    it "adds class-name if it is given unknown class-name" do
+      options = { id: "elm_id", class: "foo" }
+      merge_class_name(options, "bar")
+      expect(options[:class]).to eq "foo bar"
+    end
+
+    it "does nothing if it is given known class-name" do
+      options = { id: "elm_id", class: "foo" }
+      merge_class_name(options, "foo")
+      expect(options[:class]).to eq "foo"
+    end
+
+    it "merges class-names if it is given known class-name and unknown class-name" do
+      options = { id: "elm_id", class: "foo bar" }
+      merge_class_name(options, "foo baz")
+      expect(options[:class]).to eq "foo bar baz"
+    end
+
+    it "can receive three arguments" do
+      options = { id: "elm_id", class: "foo bar" }
+      merge_class_name(options, "foo", "baz")
+      expect(options[:class]).to eq "foo bar baz"
+    end
+  end
+end
