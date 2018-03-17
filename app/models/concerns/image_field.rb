a83599ac81a26b2531157f9ecb10120820c084fc
YuoMamoru <myuo@cam.hi-ho.ne.jp>
2018-03-17 18:54:08 +0900

    :sparkles: Create feature companies

--- app/models/concerns/image_field.rb
+++ app/models/concerns/image_field.rb
@@ -5,13 +5,19 @@ module ImageField
     fields.each do |field|
       instance_variable_name = :"@#{field}"
       define_method(field) {
-        if instance_variable_get(instance_variable_name).nil? && super()
-          instance_variable_set(instance_variable_name, Image.new(super()))
+        if instance_variable_get(instance_variable_name).nil?
+          value = super()
+          instance_variable_set(instance_variable_name, Image.new(value)) unless value.nil?
         end
+        instance_variable_get(instance_variable_name)
       }
       define_method(:"#{field}=") { |value|
-        instance_variable_set(instance_variable_name, Image.new(value))
-        super(instance_variable_get(instance_variable_name).data)
+        if value.nil?
+          super(nil)
+        else
+          instance_variable_set(instance_variable_name, Image.new(value))
+          super(instance_variable_get(instance_variable_name).data)
+        end
       }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "root#index"
 
   resources :cards, except: [ :new ]
-  resources :companies do
+  resources :companies, only: [:index, :show, :create, :update] do
     resources :cards, only: [ :new ]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
