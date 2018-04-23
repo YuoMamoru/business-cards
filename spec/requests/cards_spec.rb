ed329f02e2e905a3689304143f3649a721fa6bea
YuoMamoru <myuo@cam.hi-ho.ne.jp>
2018-04-23 17:16:41 +0900

    :white_check_mark: Add request specs

--- spec/requests/cards_spec.rb
+++ spec/requests/cards_spec.rb
@@ -3,10 +3,76 @@
 require "rails_helper"
 
 RSpec.describe "Cards", type: :request do
+  before(:all) do
+    @cards = [
+      FactoryBot.create(:card),
+      FactoryBot.create(:card),
+    ]
+    @card = @cards.first
+  end
+
+  after(:all) do
+    companies = Set.new
+    @cards.each do |card|
+      companies << card.company
+      card.destroy
+    end
+    companies.each do |company|
+      company.destroy
+    end
+  end
+
   describe "GET /cards" do
-    it "works! (now write some real specs)" do
+    it "works!" do
       get cards_path
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET /cards/new" do
+    it "works!" do
+      get new_card_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "POST /cards" do
+    attributes = FactoryBot.attributes_for(:card)
+
+    it "creates card!" do
+      expect {
+        post cards_path, params: {
+          card:  attributes.merge(company_id: @card.company_id)
+        }
+        expect(response).to have_http_status(302)
+      }.to change(Card.all, :count).by(1)
+    end
+
+    it "occors error when post data is invalid" do
+      expect {
+        post cards_path, params: {
+          card:  attributes.merge(company_id: nil)
+        }
+        expect(response).to have_http_status(200)
+      }.to change(Card.all, :count).by(0)
+    end
+  end
+
+  describe "PUT /card/:id" do
+    it "updates card!" do
+      expect {
+        put card_path(@card.id), params: { card:  @card.attributes.merge(name: "change") }
+        expect(response).to have_http_status(302)
+        expect(Card.find(@card.id).name).to_not eq @card.name
+      }.to change(Card.all, :count).by(0)
+    end
+
+    it "occors error when post data is invalid" do
+      expect {
+        put card_path(@card.id), params: { card:  @card.attributes.merge(name: nil) }
+        expect(response).to have_http_status(200)
+        expect(Card.find(@card.id).name).to eq @card.name
+      }.to change(Card.all, :count).by(0)
     end
   end
 end
--- spec/requests/companies_spec.rb
+++ spec/requests/companies_spec.rb
@@ -3,10 +3,69 @@
 require "rails_helper"
 
 RSpec.describe "Companies", type: :request do
+  before(:all) do
+    @companies = [
+      FactoryBot.create(:company_seq),
+      FactoryBot.create(:company_seq, :category_llc_before, :logo_png),
+      FactoryBot.create(:company_seq, :category_university, :logo_gif),
+      FactoryBot.create(:company_seq, :logo_jpeg),
+    ]
+    @company = @companies.first
+  end
+
+  after(:all) do
+    @companies.each do |company|
+      company.destroy
+    end
+  end
+
   describe "GET /companies" do
-    it "works! (now write some real specs)" do
+    it "works!" do
       get companies_path
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET /companies/new" do
+    it "works!" do
+      get new_company_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "POST /companies" do
+    attributes = FactoryBot.attributes_for(:company)
+
+    it "creates company!" do
+      expect {
+        post companies_path, params: { company:  attributes }
+        expect(response).to have_http_status(302)
+      }.to change(Company.all, :count).by(1)
+    end
+
+    it "occors error when post data is invalid" do
+      expect {
+        post companies_path, params: { company:  attributes.merge(name: nil) }
+        expect(response).to have_http_status(200)
+      }.to change(Company.all, :count).by(0)
+    end
+  end
+
+  describe "PUT /company/:id" do
+    it "updates company!" do
+      expect {
+        put company_path(@company.id), params: { company:  @company.attributes.merge(name: "change") }
+        expect(response).to have_http_status(302)
+        expect(Company.find(@company.id).name).to_not eq @company.name
+      }.to change(Company.all, :count).by(0)
+    end
+
+    it "occors error when post data is invalid" do
+      expect {
+        put company_path(@company.id), params: { company:  @company.attributes.merge(name: nil) }
+        expect(response).to have_http_status(200)
+        expect(Company.find(@company.id).name).to eq @company.name
+      }.to change(Company.all, :count).by(0)
     end
   end
 end
