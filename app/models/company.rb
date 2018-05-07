6ad2f9dcf860e7922695c6d0cd4960ac5272f128
YuoMamoru <myuo@cam.hi-ho.ne.jp>
2018-05-07 15:37:41 +0900

    :+1: Add company type (Limited Company)

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Company < ApplicationRecord
-  enum category: { ltd: 0x00, llc: 0x01, foundation: 0x10, university: 0x20 }
+  enum category: { ltd: 0x00, llc: 0x01, limited: 0x02, foundation: 0x10, university: 0x20 }
   enum category_position: { non: 0, before: 1, after: 2 }
   image_field :logo_image
   has_many :cards
