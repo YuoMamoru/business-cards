baea6a642ddc185e53c203fe67bd9735c5e8009a
YuoMamoru <myuo@cam.hi-ho.ne.jp>
2018-09-06 17:43:30 +0900

    :arrow_up: Upgrade MDC to 0.39.1

--- app/helpers/mdc_helper.rb
+++ app/helpers/mdc_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module MdcHelper
-  SUPPORT_VERSION = "0.35.0"
+  SUPPORT_VERSION = "0.39.1"
 
   # Create javascript include tag for {Material Components for the Web}[https://material.io/components/web/].
   def mdc_javascript_include_tag
